0b6595e1f3284acb6dc753c7efd843595788fdef
Submergechen <553056196@qq.com>
2020-08-20 21:15:52 +0800

    Initial commit

---
+++ rpc-provider/src/main/java/github/rpc/ServerBootstrap.java
@@ -0,0 +1,12 @@
+package github.rpc;
+
+
+
+//ServerBootstrap 会启动一个服务提供者，就是 NettyServer
+public class ServerBootstrap {
+    public static void main(String[] args) {
+        NettyServer nettyServer = new NettyServer();
+        //代码代填..
+        new NettyServer.startServer("127.0.0.1", 7000);
+    }
+}
